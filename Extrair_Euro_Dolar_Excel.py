b0c318ebc45709500bfbe31133d8003ebd330f16
PedroWilkinson <dev.pedromatos@gmail.com>
2023-05-31 19:13:38 -0300

    Código concluído

--- Extrair_Euro_Dolar_Excel.py
+++ Extrair_Euro_Dolar_Excel.py
@@ -73,4 +73,36 @@ valorEuroPeloGoogle = meuNavegador.find_elements(By.XPATH, '//*[@id="knowledge-c
 tempoPausaComputador.sleep(4)
 
 print(valorEuroPeloGoogle)
-print("Voce conseguiu")
+
+#----------------------------------------------------------------------------------------------------------------------------------
+
+import xlsxwriter 
+import os
+
+nomeCaminhoArquivo = "C:\\Users\\Pedro W\\Desktop\\PYTHON_GERAL\\PythonRPA\\Extraindo Valor do Dolar e Euro e Salvando no Excel\\Imprime Dolar e Euro Google.xlsx"
+planilhaCriada = xlsxwriter.Workbook(nomeCaminhoArquivo)
+sheet1 = planilhaCriada.add_worksheet()
+
+#Escervendo nas células
+sheet1.write("A1", "Dolar")
+sheet1.write("B1", "Euro")
+sheet1.write("A2", valorDolarPeloGoogle)
+sheet1.write("B2", valorEuroPeloGoogle)
+
+#Substituir a vírgula por ponto 
+valorDolarPeloGoogle = valorDolarPeloGoogle.replace(',','.')
+valorEuroPeloGoogle = valorEuroPeloGoogle.replace(',','.')
+
+#Convertendo o valor do Dolar e do Euro de Sting para Float
+valor_Dolar_Tipo_Float = float(valorDolarPeloGoogle)
+valor_Euro_Tipo_Float = float(valorEuroPeloGoogle)
+
+sheet1.write("A3", valor_Dolar_Tipo_Float)
+sheet1.write("B3", valor_Euro_Tipo_Float)
+
+#Fechando o arquivo do Excel que está em segundo plano
+planilhaCriada.close()
+
+#Abro o arquivo
+os.startfile(nomeCaminhoArquivo)
+
