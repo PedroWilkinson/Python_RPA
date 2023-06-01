f0494a067a40c7c97f405ff97a1ddb5be485cccd
PedroWilkinson <dev.pedromatos@gmail.com>
2023-06-01 19:37:50 -0300

    Concluida segunda etapa

--- Buscando_CEP_Correios_Excel.py
+++ Buscando_CEP_Correios_Excel.py
@@ -39,3 +39,43 @@ print(cidade)
 #Pega os dados da CEP no site do busca CEP pelo XPATH
 cep = navegador.find_elements(By.XPATH, '//*[@id="resultado-DNEC"]/tbody/tr/td[4]')[0].text
 print(cep)
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# SEGUNDA ETAPA DO CODIGO/PROJETO
+#------------------------------------------------------------------------------------------------------------------------------------------
+
+from openpyxl import load_workbook
+import os
+
+#Pegamos o caminho + nome do arquivo no computador
+nome_arquivo_cep = "C:\\Users\\Pedro W\\Desktop\\PYTHON_GERAL\\PythonRPA\\Extrair Endereco Site Busca CEP\\PesquisaEndereco_1.xlsx"
+planilhaDadosEndereco = load_workbook(nome_arquivo_cep)
+
+#Selecionando a sheet / planilha de Dados
+sheet_selecionada = planilhaDadosEndereco["Dados"]
+
+#Pegamos a ultima linha preenchida na coluna A e acrescentamos + 1
+linha = len(sheet_selecionada['A']) + 1
+colunaA = "A" + str(linha)  #Criando a variavel para juntar A + a ultima linha ex: A2
+colunaB = "B" + str(linha)  #Criando a variavel para juntar B + a ultima linha ex: B2
+colunaC = "C" + str(linha)  #Criando a variavel para juntar C + a ultima linha ex: C2
+colunaD = "D" + str(linha)  #Criando a variavel para juntar D + a ultima linha ex: D2
+
+#Imprimindo as informacoes do site na planilha
+sheet_selecionada[colunaA] = rua #A2
+sheet_selecionada[colunaB] = bairro #B2
+sheet_selecionada[colunaC] = cidade #C2
+sheet_selecionada[colunaD] = cep #D2
+
+#Salvando o arquivo do Excel com as novas informacoes
+planilhaDadosEndereco.save(filename=nome_arquivo_cep)
+
+#Abrindo e apresentando na tela o arquivo com os dados
+os.startfile(nome_arquivo_cep)
+
+
+
+
+
+
+
