a6fb7f6536460fd491b5e487218e82d4e7de746c
PedroWilkinson <dev.pedromatos@gmail.com>
2023-06-05 19:35:53 -0300

    Continuação do código

--- Preencher_forms_Web_Py_Excel.py
+++ Preencher_forms_Web_Py_Excel.py
@@ -5,6 +5,8 @@ import pyautogui as tempoEspera
 navegadorFormulario = opcoesSelenium.Chrome()
 navegadorFormulario.get("https://pt.surveymonkey.com/r/RD8SMQ3")
 
+tempoEspera.sleep(6)
+
 #Preenche nome
 navegadorFormulario.find_element_by_name("135842344").send_keys("Nicole Ferreira")
 
@@ -14,9 +16,19 @@ navegadorFormulario.find_element_by_name("135842392").send_keys("nicole.ferreira
 #Preenche telefone
 navegadorFormulario.find_element_by_name("135842405").send_keys("(11) 11111 - 1111")
 
-#Preenche nome
-navegadorFormulario.find_element_by_name("135842344").send_keys("Nicole Ferreira")
+#Preenche Sobre
+navegadorFormulario.find_element_by_name("135842659").send_keys("Sei automatizar processos e planilhas com Python")
 
-#Preenche Escreva Algo sobre voce
-navegadorFormulario.find_element_by_name("135842659").send_keys("Nicole Ferreira")
+
+#Preenche Radio Button Feminino
+navegadorFormulario.find_element_by_id("135842623_1011908190_label").click()
+
+tempoEspera.sleep(6)
+
+#Preenche Radio Button Feminino
+navegadorFormulario.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button').click()
+
+
+
+
 
