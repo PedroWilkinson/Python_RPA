325c9f6fa86379632b463251e5fa3b248b75a139
PedroWilkinson <dev.pedromatos@gmail.com>
2023-06-05 19:45:51 -0300

    Parte 1 Concluído

--- Preencher_forms_Web_Py_Excel.py
+++ Preencher_forms_Web_Py_Excel.py
@@ -2,31 +2,41 @@ from selenium import webdriver as opcoesSelenium
 from selenium.webdriver.common.keys import Keys
 import pyautogui as tempoEspera
 
+from selenium.webdriver.common.by import By
+
 navegadorFormulario = opcoesSelenium.Chrome()
 navegadorFormulario.get("https://pt.surveymonkey.com/r/RD8SMQ3")
 
-tempoEspera.sleep(6)
+tempoEspera.sleep(3)
 
 #Preenche nome
-navegadorFormulario.find_element_by_name("135842344").send_keys("Nicole Ferreira")
+navegadorFormulario.find_element(By.NAME, "135842344").send_keys("Nicole Ferreira")
+
+tempoEspera.sleep(3)
 
 #Preenche email
-navegadorFormulario.find_element_by_name("135842392").send_keys("nicole.ferreira@gmail.com")
+navegadorFormulario.find_element(By.NAME, "135842392").send_keys("nicole.ferreira@gmail.com")
+
+tempoEspera.sleep(3)
 
 #Preenche telefone
-navegadorFormulario.find_element_by_name("135842405").send_keys("(11) 11111 - 1111")
+navegadorFormulario.find_element(By.NAME, "135842405").send_keys("(11) 11111 - 1111")
+
+tempoEspera.sleep(3)
 
 #Preenche Sobre
-navegadorFormulario.find_element_by_name("135842659").send_keys("Sei automatizar processos e planilhas com Python")
+navegadorFormulario.find_element(By.NAME, "135842659").send_keys("Sei automatizar processos e planilhas com Python")
+
+tempoEspera.sleep(3)
 
 
 #Preenche Radio Button Feminino
-navegadorFormulario.find_element_by_id("135842623_1011908190_label").click()
+navegadorFormulario.find_element(By.ID, "135842623_1011908190_label").click()
 
-tempoEspera.sleep(6)
+tempoEspera.sleep(3)
 
-#Preenche Radio Button Feminino
-navegadorFormulario.find_element_by_xpath('//*[@id="patas"]/main/article/section/form/div[2]/button').click()
+#Clica para enviar as informações
+navegadorFormulario.find_element(By.XPATH, '//*[@id="patas"]/main/article/section/form/div[2]/button').click()
 
 
 
