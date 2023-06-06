f502423b24c0d2906a285d1c3e7be15444d43f2c
PedroWilkinson <dev.pedromatos@gmail.com>
2023-06-06 19:45:17 -0300

    Código Concluído

--- Preencher_forms_Web_Py_Excel.py
+++ Preencher_forms_Web_Py_Excel.py
@@ -7,47 +7,6 @@ import pyautogui as tempoEspera
 
 from selenium.webdriver.common.by import By
 
-'''
-navegadorFormulario = opcoesSelenium.Chrome()
-navegadorFormulario.get("https://pt.surveymonkey.com/r/RD8SMQ3")
-
-tempoEspera.sleep(3)
-
-#Preenche nome
-navegadorFormulario.find_element(By.NAME, "135842344").send_keys("Nicole Ferreira")
-
-tempoEspera.sleep(3)
-
-#Preenche email
-navegadorFormulario.find_element(By.NAME, "135842392").send_keys("nicole.ferreira@gmail.com")
-
-tempoEspera.sleep(3)
-
-#Preenche telefone
-navegadorFormulario.find_element(By.NAME, "135842405").send_keys("(11) 11111 - 1111")
-
-tempoEspera.sleep(3)
-
-#Preenche Sobre
-navegadorFormulario.find_element(By.NAME, "135842659").send_keys("Sei automatizar processos e planilhas com Python")
-
-tempoEspera.sleep(3)
-
-
-#Preenche Radio Button Feminino
-navegadorFormulario.find_element(By.ID, "135842623_1011908190_label").click()
-
-tempoEspera.sleep(3)
-
-#Clica para enviar as informações
-navegadorFormulario.find_element(By.XPATH, '//*[@id="patas"]/main/article/section/form/div[2]/button').click()
-'''
-#--------------------------------------------------------------------------------------------------------------------
-# SEGUNDA PARTE
-#--------------------------------------------------------------------------------------------------------------------
-
-
-
 nomeCaminhoArquivo = "C:\\Users\\Pedro W\Desktop\\PYTHON_GERAL\\PythonRPA\\Preenchendo Formulários na Web com Python e Excel\\DadosFormulario.xlsx"
 planilha_aberta = load_workbook(nomeCaminhoArquivo)
 
