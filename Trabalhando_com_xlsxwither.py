96e71c4a5861314272045ccfec8f316d21ac8f75
PedroWilkinson <dev.pedromatos@gmail.com>
2023-06-08 13:10:11 -0300

    Alterando Cor Fonte e Fonte da Celula

--- Trabalhando_com_xlsxwither.py
+++ Trabalhando_com_xlsxwither.py
@@ -1,21 +1,29 @@
 import xlsxwriter as opcoesDoXlsxWriter
 import os
 
-nomeCaminhoArquivo = 'C:\\Users\\Pedro W\\Desktop\\PYTHON_GERAL\\PythonRPA\\Xlsxwriter\\PrimeiroExemplo.xlsx'
-workbook = opcoesDoXlsxWriter.Workbook(nomeCaminhoArquivo)
-sheetPadrao = workbook.add_worksheet()
+nomeCaminhoArquivo = 'C:\\Users\\Pedro W\\Desktop\\PYTHON_GERAL\\PythonRPA\\Xlsxwriter\\PintaFundoEFonte.xlsx'
+minhaPlanilha = opcoesDoXlsxWriter.Workbook(nomeCaminhoArquivo)
+
+sheetDados = minhaPlanilha.add_worksheet("Dados")
+
+#Altera a cor do fundo da celula
+corFundo = minhaPlanilha.add_format({'fg_color':'yellow'})
+
+#Altera a cor da fonte
+corFonte = minhaPlanilha.add_format()
+corFonte.set_font_color('blue')
 
 #Adicionando dados na Sheet
-sheetPadrao.write("A1", "Nome")
-sheetPadrao.write("B1", "Idade")
-sheetPadrao.write("A2", "Amanda")
-sheetPadrao.write("B2", 21)
-sheetPadrao.write("A3", "Pedro")
-sheetPadrao.write("B3", 28)
+sheetDados.write("A1", "Nome", corFundo)
+sheetDados.write("B1", "Idade", corFundo)
+sheetDados.write("A2", "Amanda", corFonte)
+sheetDados.write("B2", 21, corFonte)
+sheetDados.write("A3", "Pedro", corFonte)
+sheetDados.write("B3", 28, corFonte)
 
 
 #Fechando o arquivo
-workbook.close()
+minhaPlanilha.close()
 
 
 #Abrindo o arquivo
